c556af9229bcd6a0435bdcfa34d57bf21e34aab6
oneminute <oneminute3b@gmail.com>
2020-10-26 14:32:27 +0800

    1. Build fold structure. 2. Complete CMakeLists.txt.

---
+++ widget/EditSlider.cpp
@@ -0,0 +1,185 @@
+#include "EditSlider.h"
+
+#include <QBoxLayout>
+#include <QtMath>
+
+EditSlider::EditSlider(Qt::Orientation dir, QWidget *parent) :
+    QWidget(parent)
+{
+    m_slider = new QSlider(dir);
+
+    m_edit = new QLineEdit;
+    m_edit->setMaximumWidth(30);
+    m_edit->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Preferred);
+    m_edit->setText(QString::number(m_slider->minimum()));
+
+    m_validator = new QIntValidator(m_slider->minimum(), m_slider->maximum(), this);
+    m_edit->setValidator(m_validator);
+
+    QBoxLayout* layout = new QBoxLayout(QBoxLayout::Direction::LeftToRight);
+    if (dir == Qt::Horizontal)
+        layout->setDirection(QBoxLayout::LeftToRight);
+    else if (dir == Qt::Vertical)
+        layout->setDirection(QBoxLayout::TopToBottom);
+
+    layout->addWidget(m_slider);
+    layout->addWidget(m_edit);
+    layout->setStretch(0, 1);
+    layout->setStretch(1, 0);
+    layout->setMargin(0);
+
+    setLayout(layout);
+
+    connect(m_slider, &QSlider::valueChanged, this, &EditSlider::onSliderValueChanged);
+    connect(m_edit, &QLineEdit::textChanged, this, &EditSlider::onLineEditTextChanged);
+    connect(m_slider, &QSlider::valueChanged, this, &EditSlider::valueChanged);
+}
+
+int EditSlider::minimum() const
+{
+    return m_slider->minimum();
+}
+
+void EditSlider::setMinimum(int minimum)
+{
+    m_slider->setMinimum(minimum);
+    m_validator->setRange(m_slider->minimum(), m_slider->maximum());
+    validateText();
+}
+
+int EditSlider::maximum() const
+{
+    return m_slider->maximum();
+}
+
+void EditSlider::setMaximum(int maximum)
+{
+    m_slider->setMaximum(maximum);
+    m_validator->setRange(m_slider->minimum(), m_slider->maximum());
+    validateText();
+}
+
+int EditSlider::singleStep() const
+{
+    return m_slider->singleStep();
+}
+
+void EditSlider::setSingleStep(int singleStep)
+{
+    m_slider->setSingleStep(singleStep);
+}
+
+int EditSlider::pageStep() const
+{
+    return m_slider->pageStep();
+}
+
+void EditSlider::setPageStep(int pageStep)
+{
+    m_slider->setPageStep(pageStep);
+}
+
+int EditSlider::value() const
+{
+    return m_slider->value();
+}
+
+void EditSlider::setValue(int value)
+{
+    m_slider->setValue(value);
+    m_edit->blockSignals(true);
+    m_edit->setText(QString::number(value));
+    m_edit->blockSignals(false);
+
+    emit valueChanged(value);
+}
+
+int EditSlider::sliderPosition() const
+{
+    return m_slider->sliderPosition();
+}
+
+void EditSlider::setSliderPosition(int pos)
+{
+    m_slider->setSliderPosition(pos);
+}
+
+bool EditSlider::tracking() const
+{
+    return m_slider->hasTracking();
+}
+
+void EditSlider::setTracking(bool tracking)
+{
+    m_slider->setTracking(tracking);
+}
+
+QSize EditSlider::maximumLineEditSize() const
+{
+    return m_edit->maximumSize();
+}
+
+void EditSlider::setMaximumLineEditSize(const QSize &size)
+{
+    m_edit->setMaximumSize(size);
+}
+
+QString EditSlider::text() const
+{
+    return m_edit->text();
+}
+
+void EditSlider::setText(const QString &text)
+{
+    int pos = 0;
+    QString str = text;
+    if (m_validator->validate(str, pos) == QValidator::Acceptable)
+    {
+        m_edit->setText(text);
+        int value = text.toInt();
+        m_slider->blockSignals(true);
+        m_slider->setValue(value);
+        m_slider->blockSignals(false);
+
+        emit valueChanged(value);
+    }
+}
+
+void EditSlider::onLineEditSizeChanged()
+{
+}
+
+void EditSlider::onSliderRangeChanged()
+{
+}
+
+void EditSlider::onTextChanged()
+{
+}
+
+void EditSlider::onSliderValueChanged(int value)
+{
+    m_edit->blockSignals(true);
+    m_edit->setText(QString::number(value));
+    m_edit->blockSignals(false);
+}
+
+void EditSlider::onLineEditTextChanged(const QString &text)
+{
+    QString str = text;
+    int pos = 0;
+    if (m_validator->validate(str, pos) == QValidator::Acceptable)
+    {
+        m_slider->blockSignals(true);
+        int value = qBound(m_slider->minimum(), text.toInt(), m_slider->maximum());
+        m_slider->setValue(value);
+        m_slider->blockSignals(false);
+    }
+}
+
+void EditSlider::validateText()
+{
+    int value = m_edit->text().toInt();
+    value = qBound(m_slider->minimum(), value, m_slider->maximum());
+    m_edit->setText(QString::number(value));
+}
---
+++ widget/EditSlider.h
@@ -0,0 +1,74 @@
+#ifndef EDITSLIDER_H
+#define EDITSLIDER_H
+
+#include <QWidget>
+#include <QLineEdit>
+#include <QSlider>
+#include <QIntValidator>
+#include <QtUiPlugin/QDesignerExportWidget>
+
+class EditSlider : public QWidget
+{
+    Q_OBJECT
+
+    Q_PROPERTY(int minimum READ minimum WRITE setMinimum NOTIFY onSliderRangeChanged DESIGNABLE true)
+    Q_PROPERTY(int maximum READ maximum WRITE setMaximum NOTIFY onSliderRangeChanged DESIGNABLE true)
+    Q_PROPERTY(int singleStep READ singleStep WRITE setSingleStep DESIGNABLE true)
+    Q_PROPERTY(int pageStep READ pageStep WRITE setPageStep DESIGNABLE true)
+    Q_PROPERTY(int value READ value WRITE setValue DESIGNABLE true)
+    Q_PROPERTY(int sliderPosition READ sliderPosition WRITE setSliderPosition DESIGNABLE true)
+    Q_PROPERTY(bool tracking READ tracking WRITE setTracking DESIGNABLE true)
+
+    Q_PROPERTY(QString text READ text WRITE setText NOTIFY onTextChanged DESIGNABLE false)
+    Q_PROPERTY(QSize maximumLineEditSize READ maximumLineEditSize WRITE setMaximumLineEditSize NOTIFY onLineEditSizeChanged DESIGNABLE true)
+public:
+    EditSlider(Qt::Orientation dir = Qt::Horizontal, QWidget *parent = 0);
+
+    int minimum() const;
+    void setMinimum(int minimum);
+
+    int maximum() const;
+    void setMaximum(int maximum);
+
+    int singleStep() const;
+    void setSingleStep(int singleStep);
+
+    int pageStep() const;
+    void setPageStep(int pageStep);
+
+    int value() const;
+    void setValue(int value);
+
+    int sliderPosition() const;
+    void setSliderPosition(int pos);
+
+    bool tracking() const;
+    void setTracking(bool tracking);
+
+    QSize maximumLineEditSize() const;
+    void setMaximumLineEditSize(const QSize& size);
+
+    QString text() const;
+    void setText(const QString& text);
+
+public slots:
+    void onLineEditSizeChanged();
+    void onSliderRangeChanged();
+    void onTextChanged();
+
+    void onSliderValueChanged(int value);
+    void onLineEditTextChanged(const QString& text);
+
+signals:
+    void valueChanged(int value);
+
+private:
+    void validateText();
+
+private:
+    QLineEdit* m_edit;
+    QSlider* m_slider;
+    QIntValidator* m_validator;
+};
+
+#endif // EDITSLIDER_H
