f6150c464b2bd5e31c5704cc12ea6771314a2e08
oneminute <oneminute3b@gmail.com>
2020-10-26 15:54:12 +0800

    Add export definition macro.

---
+++ ExportLRWidgets.h
@@ -0,0 +1,15 @@
+#ifndef EXPORTLRWIDGETS_H
+#define EXPORTLRWIDGETS_H
+
+#if defined(BUILD_SHARED_LIBRARY)
+#   if defined(EXPORT_LRWIDGETS)
+#       define LRWIDGET_EXPORT Q_DECL_EXPORT
+#   else
+#       define LRWIDGET_EXPORT Q_DECL_IMPORT
+#   endif
+#else
+#   define LRWIDGET_EXPORT Q_DECL_IMPORT
+#endif
+
+
+#endif // EXPORTLRWIDGETS_H
