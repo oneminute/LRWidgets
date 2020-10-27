755d5e489985f2638120b10591a568907089e1e3
sun <329937626@qq.com>
2020-10-27 10:32:52 +0800

    imagebutton

---
+++ widget/imagebutton.cpp
@@ -0,0 +1,55 @@
+﻿#include "imagebutton.h"
+#include "ui_imagebutton.h"
+#include <QDebug>
+#include <QFont>
+ImageButton::ImageButton(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::ImageButton)
+{
+    ui->setupUi(this);
+    //ui->img->setMaximumSize(150, 150);
+    ui->img->setAlignment(Qt::AlignCenter);
+
+
+    QPixmap pix(":/imgs/images/icon.png");
+    //ui->img->setScaledContents(true);
+    //ui->img->resize(QSize(60, 60));
+
+    //pix.scaled(QSize(60, 60), Qt::IgnoreAspectRatio);
+
+    ui->img->setPixmap(pix);
+    ui->text->setText("123");
+    //ui->text->resize(ui->img->width(), ui->text->height());
+
+
+    ui->text->adjustSize();
+    QFont textFont("Helvetica");
+    textFont.setPointSize(12);
+    ui->text->setFont(textFont);
+
+    //ui->text->setAlignment(Qt::AlignHCenter);
+    //qDebug()<<ui->text->fontInfo().pointSize()<<endl;
+    QPalette pe;
+    //设置黑底红字
+    pe.setColor(QPalette::Background,Qt::black);
+    pe.setColor(QPalette::WindowText,Qt::red);
+    ui->text->setAutoFillBackground(true);
+    //ui->text->setPalette(pe);
+
+
+    //ui->verticalLayout->setGeometry(QRect(0, 0, 50, 50));
+    //this->setGeometry(0, 0, 50, 50);
+    //qDebug()<<this->width()<<endl;
+}
+
+ImageButton::~ImageButton()
+{
+    delete ui;
+}
+
+void ImageButton::setImagePath(const char* _path)
+{
+    QPixmap pix(":/imgs/images/chart.png");
+    ui->img->setPixmap(pix);
+
+}
---
+++ widget/imagebutton.h
@@ -0,0 +1,26 @@
+﻿#ifndef IMAGEBUTTON_H
+#define IMAGEBUTTON_H
+
+#include <QWidget>
+/**
+ *使用时，需要在界面中添加一个和此自定义控件一样大小的widget（60*80）
+ * 图片的尺寸为50*50
+ */
+namespace Ui {
+class ImageButton;
+}
+
+class ImageButton : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ImageButton(QWidget *parent = 0);
+    ~ImageButton();
+    void setImagePath(const char* _path);
+
+private:
+    Ui::ImageButton *ui;
+};
+
+#endif // IMAGEBUTTON_H
