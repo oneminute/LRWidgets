72b2bb483493d76f0f242997ba4645da8348d337
oneminute <oneminute3b@gmail.com>
2020-10-28 14:33:09 +0800

    Move install scripts into if-endif block.

--- translation/LRWidgets_zh_CN.ts
+++ translation/LRWidgets_zh_CN.ts
@@ -1,4 +1,15 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
+<context>
+    <name>ImageButton</name>
+    <message>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
